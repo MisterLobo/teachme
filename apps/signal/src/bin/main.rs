663084da5ef335a2fe973b91798b43cc5cd733f1
MisterLobo <algae1234567890@gmail.com>
2026-05-23 01:55:36 +0800

    signal: update main

--- apps/signal/src/bin/main.rs
+++ apps/signal/src/bin/main.rs
@@ -2,15 +2,23 @@ use std::env;
 
 use loco_rs::cli;
 use migration::Migrator;
-use qdrant_client::{Qdrant, qdrant::{CreateCollectionBuilder, VectorParams, VectorParamsBuilder, VectorsConfigBuilder}};
+use qdrant_client::{Qdrant, qdrant::{CreateCollectionBuilder, VectorParamsBuilder, VectorsConfigBuilder}};
+use rustls::crypto::CryptoProvider;
 use signal::app::App;
 use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() -> loco_rs::Result<()> {
     dotenv().ok();
-    let client = Qdrant::from_url(&env::var("QDRANT_API_URL").unwrap_or_default()).skip_compatibility_check().build().expect("failed to initialize connection");
-    // client.config.set_api_key("");
+
+    CryptoProvider::install_default(
+        rustls::crypto::ring::default_provider()
+    ).unwrap();
+
+    let client = Qdrant::from_url(&env::var("QDRANT_API_URL").unwrap_or_default())
+        .api_key(env::var("QDRANT_API_KEY"))
+        .build()
+        .expect("failed to initialize connection");
     let coll = client.list_collections().await.expect("error");
     let params = VectorParamsBuilder::new(384, qdrant_client::qdrant::Distance::Cosine).build();
     let mut config = VectorsConfigBuilder::default();
